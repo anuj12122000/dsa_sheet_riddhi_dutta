8b59467763273dbe208a17dfcb5d7fa550d38879
anuj12122000 <62623809+anuj12122000@users.noreply.github.com>
2022-06-04 01:27:35 +0530

    first commit

---
+++ Day2/tapping_rain_water.cpp
@@ -0,0 +1,38 @@
+int trap(vector<int>& height) {
+        int left=0;
+        int right = height.size()-1;
+        int left_max = 0;
+        int right_max=0;
+        int res = 0;
+
+
+        while(left<=right){
+
+                if(left_max<=right_max){
+
+                        if(height[left]>left_max){
+                            left_max = height[left];
+                        }else{
+                            res+= left_max-height[left]; 
+                        }
+                    left++;
+
+                }else{
+
+                        if(height[right]>right_max){
+                            right_max=height[right];
+                        }else{
+                            res+=right_max-height[right];
+                        }
+
+
+                        right--;
+                }
+
+
+
+        }
+
+
+        return res;
+    }
