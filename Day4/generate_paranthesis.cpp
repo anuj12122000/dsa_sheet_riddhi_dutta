190ad28a53fe381e207a127e8293bb8b8ab35041
anuj12122000 <62623809+anuj12122000@users.noreply.github.com>
2022-06-06 22:19:52 +0530

    first commit

---
+++ Day4/generate_paranthesis.cpp
@@ -0,0 +1,34 @@
+class Solution {
+
+private:
+
+    void helper(int open , int close , string s, vector<string>&res){
+
+   
+        if(open==0 && close==0){
+            res.push_back(s);
+            return ;
+        }
+
+        
+        if(open>0){
+            helper(open-1,close,s+'(',res);
+        }
+
+        if(close>open)
+        helper(open,close-1,s+')',res);
+
+        
+
+    }
+
+
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string>res;
+        int open  = n;
+        int close = n;
+        helper(open,close, "",res);
+        return res;
+    }
+};
